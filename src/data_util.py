ce9285c90c105f3c6c81f0c9743cf2870240d3a4
zaracs <whqian001@outlook.com>
2020-09-04 22:04:14 +0800

    change data processing

--- src/data_util.py
+++ src/data_util.py
@@ -1,4 +1,4 @@
-import random
+import random, time
 import numpy as np
 from vocab import PLH_ID, PLH, PAD_ID
 
@@ -13,19 +13,80 @@ def align_texts(texts, pad_id=PAD_ID, pad_mask=False):
     else:
         return texts
 
+# ===============================================================
+
+def mask_noise_file(input_file, output_file, label, p):
+    lines = [line.strip() for line in open(input_file, 'r')]
+    noised_lines = [" ".join(mask_noise_text(line, p, PLH)) for line in lines]
+    with open(output_file, 'w+') as f:
+        for l, nl in zip(lines, noised_lines):
+            f.write('\t'.join([l, nl, str(label)]) + "\n")
+
+def mask_noise_text(text, p, mask_token):
+    text = np.array(text)
+    up = np.random.uniform(size=len(text))
+    text[up < p] = mask_token
+    return text.tolist()
+
+def iter_samples(origin, ids):
+    ms, s, ms_filter, idt = [], [], [], []
+    origin, ids = np.array(origin), np.array(ids)
+    while True:
+        inds = (ids == PAD_ID).astype(np.int) #TODO:
+        mask = ((inds - (np.concatenate([inds[-1:], inds[:-1]]))) > 0).astype(np.int)
+        print(mask.sum())
+        if mask.sum() == 0:
+            ms_filter.append(origin.tolist())
+            idt.append([0] * (origin.size - 1) + [1])
+            break
+        else:
+            a, b = filter_right_PLH_id(origin, ids)
+            c, d = remove_PLH_id(a)
+            ms.append(a)
+            s.append(b)
+            ms_filter.append(c)
+            idt.append(d)
+        time.sleep(0.5)
+        ids = (mask * origin) + (1 - inds) * ids
+    return ms, s, ms_filter, idt
+    
+
+def filter_right_PLH_id(origin, ids):
+    new_ids, output, flag = [], [], 0
+    for oid, id_ in zip(origin, ids):
+        if id_ != PAD_ID: #TODO:
+            new_ids.append(id_)
+            output.append(oid)
+            flag = 0
+        else:
+            if flag == 0:
+                new_ids.append(id_)
+                output.append(oid)
+                flag = 1
+    return new_ids, output
+
+def remove_PLH_id(ids):
+    new_ids, inds, flag = [], [], 0
+    for id_ in ids[::-1]:
+        if id_ == PAD_ID: #TODO:
+            flag = 1
+        else:
+            new_ids.insert(0, id_,)
+            if flag == 1:
+                inds.insert(0, 1)
+                flag = 0
+            else:
+                inds.insert(0, 0)
+    return new_ids, inds
+
+
+# ===============================================================
 
 def noise_text_ids(texts, p, noise_id, noise_type):
     pad_texts = align_texts(texts)
     noise_texts = [noise_text_ids_(text, p, noise_id, noise_type) for text in texts]
     noise_texts = align_texts(noise_texts)
     return pad_texts, noise_texts
-
-
-def noise_text(text, p, mask_token):
-    text = np.array(text)
-    up = np.random.uniform(size=len(text))
-    text[up < p] = mask_token
-    return text.tolist()
 
 def noise_text_ids_(text, p, noise_id, noise_type):
     new_text = []
@@ -45,3 +106,16 @@ def noise_text_ids_(text, p, noise_id, noise_type):
         else:
             new_text.append(word)
     return new_text
+
+
+if __name__ == "__main__":
+    a = [100, 1, 2, 3, 4, 100]
+    b = [100, 0, 0, 1, 0, 100]
+    print(a)
+    print(b)
+    print('=' * 100)
+    a, b, c, d = iter_samples(a, b)
+    print(a)
+    print(b)
+    print(c)
+    print(d)
--- src/dataset.py
+++ src/dataset.py
@@ -6,7 +6,7 @@ import numpy as np
 from vocab import PAD_ID, BOS_ID, EOS_ID, PLH_ID, Vocab
 from torch.utils.data import Dataset
 
-from data_util import align_texts, noise_text_ids
+from data_util import align_texts, noise_text_ids, filter_right_PLH_id, remove_PLH_id
 
 
 class StyleDataset(Dataset):
@@ -79,7 +79,7 @@ class TemplateDataset(Dataset):
         return samples
     
     def __load_lines(self, f_obj):
-        encode = lambda s: self.vocab.tokens_to_ids(s)[: self.max_len]
+        encode = lambda s: [BOS_ID] + self.vocab.tokens_to_ids(s)[: self.max_len] + [EOS_ID]
         # load
         sentences = [line.strip().split("\t") for line in f_obj]
         return [(encode(s.split()), encode(s_temp.split()), int(label)) for s, s_temp, label in sentences]
@@ -91,17 +91,21 @@ class TemplateDataset(Dataset):
         return len(self.samples)
     
     @staticmethod
-    def collate_fn_rank(batch_samples):
-        sentences, temp_sentences, labels = zip(*batch_samples)
-        aligned_sentences = torch.tensor(align_texts(sentences), dtype=torch.long)
-        aligned_temps = torch.tensor(align_texts(temp_sentences), dtype=torch.long)
-        ph_mask = (aligned_temps == PLH_ID).long()
-        labels = torch.tensor(labels, dtype=torch.long)
+    def collate_fn_train(batch_samples):
+        for masked_ids, ids, removed_ids, inds = [], [], [], []
+        for s, ns, l in  zip(*batch_samples):
+            ms, s = filter_right_PLH_id(s, ns)
+            fs, i = remove_PLH_id(ms)
+
+        # aligned_sentences = torch.tensor(align_texts(sentences), dtype=torch.long)
+        # aligned_temps = torch.tensor(align_texts(temp_sentences), dtype=torch.long)
+        # ph_mask = (aligned_temps == PLH_ID).long()
+        # labels = torch.tensor(labels, dtype=torch.long)
 
         return aligned_sentences, aligned_temps, ph_mask, labels
     
     @staticmethod
-    def collate_fn_transfer(batch_samples):
+    def collate_fn_inf(batch_samples):
         sentences, temp_sentences, labels = zip(*batch_samples)
         sentences = [[BOS_ID] + s + [EOS_ID] for s in sentences]
         temp_sentences = [[BOS_ID] + s + [EOS_ID] for s in temp_sentences]
@@ -111,70 +115,3 @@ class TemplateDataset(Dataset):
         aligned_temps = torch.tensor(align_texts(temp_sentences), dtype=torch.long)
         labels = torch.tensor(labels, dtype=torch.long)
         return aligned_sentences, aligned_temps, labels
-
-
-class InsertLMDataset(Dataset):
-    def __init__(self, files, vocab, max_len=None):
-        super().__init__()
-        self.files = files
-        self.vocab = vocab
-        self.max_len = max_len
-        self.samples = self._load()
-
-    def _load(self):
-        samples = []
-        for file in self.files:
-            assert os.path.exists(file)
-            f_obj = open(file, 'r', encoding='utf-8')
-            samples += self.__load_lines(f_obj)
-            f_obj.close()
-        return samples
-    
-    def __load_lines(self, f_obj):
-        samples = []
-        encode = lambda s: self.vocab.tokens_to_ids(s)[: self.max_len]
-        for line in f_obj:
-            text, order, label = line.strip().split("\t")
-            tokens = [BOS_ID] + encode(text.strip().split()) + [EOS_ID]
-            order = [0] + [int(od) for od in order.split()] + [0]
-            label = int(label)
-            samples += self.___segments(tokens, order, label)
-        return samples
-    
-    def ___segments(self, tokens, order, label):
-        t, p = np.array(tokens), np.array(order)
-        samples = []
-        for i in range((p > 0).sum() + 1):
-            step = i + 1
-            m1, m2 = (p < step), (p == step)
-            out = m1 * t
-            pre = 2 * m2 + m1 - 1
-            if step > 1:
-                m3 = (p == (step - 1))
-                inp = m3 * PLH_ID + (1 - m3) * out
-            else:
-                inp = out
-            inp = list(filter(lambda x: x > 0, inp))
-            out = list(filter(lambda x: x > 0, out))
-            pre = list(filter(lambda x: x != -1, pre))[:len(inp)]
-            if sum(pre) == 0:
-                pre[0] = 1
-            assert len(inp) == len(out)
-            assert len(inp) == len(pre)
-            samples.append((inp, out, pre, label))
-        return samples
-    
-    def __getitem__(self, idx):
-        return self.samples[idx]
-    
-    def __len__(self):
-        return len(self.samples)
-    
-    @staticmethod
-    def collate_fn(batch_samples):
-        inp, out, pos, label = zip(*batch_samples)
-        inp = torch.tensor(align_texts(inp), dtype=torch.long)
-        out = torch.tensor(align_texts(out), dtype=torch.long)
-        pos = torch.tensor(align_texts(pos, pad_id=0), dtype=torch.float)
-        label = torch.tensor(label, dtype=torch.long)
-        return inp, out, pos, label
