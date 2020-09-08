4dd5497fb42d6b96dca8102600c24f9653eaa785
zaracs <whqian001@outlook.com>
2020-09-08 09:57:52 +0800

    add test

---
+++ src/test.py
@@ -0,0 +1,23 @@
+import torch
+from vocab import Vocab, PAD_ID, PLH_ID
+from nets.classifier import TextCNN
+
+dev = torch.device("cuda:0")
+
+vocab = Vocab.load("../dump/vocab_yelp.bin")
+
+model = TextCNN(len(vocab)).to(dev)
+model.load_state_dict(torch.load("../dump/clf_yelp.pth"))
+
+
+while True:
+    query = input("Input a sentence: ")
+    tokens = query.strip().split()
+    ids = vocab.tokens_to_ids(tokens)
+    src = torch.tensor(ids).long().to(dev)
+    ids = list(map(lambda x: x if x != PLH_ID else PAD_ID))
+    tgt = torch.tensor(ids).long().to(dev)
+
+    with torch.no_grad():
+        res = model(torch.stack([src, tgt], dim=0))
+        print(res.cpu().numpy())
