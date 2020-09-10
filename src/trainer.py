82b452af352875bfadfb41c12e0e5ab72f85789b
zaracs <whqian001@outlook.com>
2020-09-10 16:30:39 +0800

    insert -> mask

--- src/trainer.py
+++ src/trainer.py
@@ -79,9 +79,9 @@ class MaskTrainer:
             x, x_, y = embed_device([x, x_, y], self.dev)
             pred = self.clf(x_)
             # weighted loss
-            # weights = (x_ != PAD_ID).sum(1).float() / (x != PAD_ID).sum(1).float()
+            weights = (x_ != PAD_ID).sum(1).float() / (x != PAD_ID).sum(1).float()
 
-            loss_cls_N = self.bce(pred, y.float())# * weights
+            loss_cls_N = self.bce(pred, y.float()) * weights
             loss_cls = loss_cls_N.mean()
 
             self.optimize_clf_0.zero_grad()
