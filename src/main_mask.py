3ae244ff98ec646f21337ed7a2b12fc573f55d2c
zaracs <whqian001@outlook.com>
2020-11-04 10:40:11 +0800

    finetune

--- src/main_mask.py
+++ src/main_mask.py
@@ -31,12 +31,12 @@ logger.info(f"mode: {mode}")
 
 # parameters
 #=============================================================#
-epochs_clf = 20
+epochs_clf = 10
 epochs_masker = 40
 batch_size = 512
 max_seq_len = None # no limit
-noise_p = 0.2
-delta = 0.7
+noise_p = 0.15
+delta = 0.65
 
 rollouts = 8
 gamma = 0.85
