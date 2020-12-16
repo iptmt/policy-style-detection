1d748c678085773ae9dda5622bbeeea460ac87a7
zaracs <whqian001@outlook.com>
2020-12-16 19:33:11 +0800

    v1

--- src/main_mask.py
+++ src/main_mask.py
@@ -70,7 +70,7 @@ if mode == "train":
     # construct trainer
     #=============================================================#
     optimize_clf = torch.optim.Adam(clf.parameters(), lr=1e-3)
-    optimize_masker = torch.optim.Adam(masker.parameters(), lr=3e-4)
+    optimize_masker = torch.optim.Adam(masker.parameters(), lr=1e-4)
     model_trainer = MaskTrainer(masker, clf, dev, rollouts, gamma, optimize_masker, optimize_clf)
     #=============================================================#
 
