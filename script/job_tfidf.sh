e5db4cc7f550a009d731fd87026dd2eda82f3b6a
zaracs <whqian001@outlook.com>
2020-12-23 01:55:38 +0800

    finish TG eval

--- script/job_tfidf.sh
+++ script/job_tfidf.sh
@@ -7,11 +7,11 @@ if [ ! -f $log_f  ]; then
     touch $log_f
 fi
 
-for i in $(seq 0.8 0.02 0.98)
+for i in $(seq 0.25 0.0611111 0.8)
 do
     echo $i >> $log_f
     cd /home/zaracs/workspace/baselines/tagger-generator/tag-and-generate-data-prep
-    python src/run.py --data_pth ../data/$ds.tsv --outpath ../data/ --style_0_label P_0 --style_1_label P_9 --is_unimodal False --thresh $i
+    python src/run.py --data_pth ../data/$ds.tsv --outpath ../data/ --style_0_label P_9 --style_1_label P_0 --is_unimodal True --thresh $i
 
     cd /home/zaracs/workspace/policy-style-detection/script
     python eval_tg.py $ds
