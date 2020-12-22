068b05bc51dcbce77b927d4738e705c534ec6f0a
zaracs <whqian001@outlook.com>
2020-12-22 16:20:15 +0800

    evaluation

---
+++ script/eval_both.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+./eval.sh yelp ../out/$1/yelp_test.tsf
+./eval.sh gyafc ../out/$1/gyafc_test.tsf
--- script/test.py
+++ script/test.py
@@ -1,8 +1,36 @@
+import sys
 from fio import read, write
 
-topic = "caae"
 
-lines = read(f"../out/{topic}/gyafc_test.tsf")
+def reorg(file_list, out, order): # order: [0, 2, 1]
+    multi_lines = [read(f) for f in file_list]
+    line_tuples = list(zip(*multi_lines))
+    line_tuples = [[e for ele in line_tuple for e in ele.strip().split("\t")] for line_tuple in line_tuples]
+    re_order_tuples = [[tp[order[i]].strip() for i in range(len(order))] for tp in line_tuples]
+    lines = ["\t".join(tp) for tp in re_order_tuples]
+    write(out, lines)
 
-lines = [line + "\t" + "1" for line in lines]
-write(f"../out/{topic}/gyafc_test.tsf", lines)
+def append(inp, out, label):
+    lines = read(inp)
+    lines = [l.strip() + "\t" + str(label) for l in lines]
+    write(out, lines)
+
+def check(inp, out):
+    lines = read(inp)
+    lines = [l.split("\t") for l in lines]
+    new_lines = []
+    for line in lines:
+        if len(line) < 3:
+            line = [line[0], ".", line[1]]
+        new_lines.append("\t".join(line))
+    write(out, new_lines)
+
+
+
+if __name__ == "__main__":
+    model = "tg"
+    data = "gyafc"
+
+    reorg([f"../data/gyafc/style.test.0", f"../out/{model}/{data}_test.tsf"], f"../out/{model}/{data}_test.tsf", [0, 1])
+    append(f"../out/{model}/{data}_test.tsf", f"../out/{model}/{data}_test.tsf", "1")
+    # check(f"../out/{model}/{data}_test.tsf", f"../out/{model}/{data}_test.tsf")
