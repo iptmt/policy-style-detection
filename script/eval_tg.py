bdea7d8ea5a76710b71767db027a04d24f32dd47
zaracs <whqian001@outlook.com>
2020-12-23 00:39:00 +0800

    eval tg

--- script/eval_tg.py
+++ script/eval_tg.py
@@ -6,7 +6,7 @@ def replace(text):
     return re.sub(r"\[.*?\]", "<PH>", text)
 
 tmp = "/home/zaracs/workspace/policy-style-detection/tmp/"
-base = "/home/zaracs/workspace/baselines/tagger-generator/tag-and-generate-data-prep/data/"
+base = "/home/zaracs/workspace/baselines/tagger-generator/data/"
 f0 = "entagged_parallel.test.en.P_0"
 f1 = "entagged_parallel.test.en.P_9"
 
@@ -16,4 +16,4 @@ f1_ = "entagged_parallel.test.tagged.P_9"
 tt0 = [x + "\t" + replace(x_) + "\t" + "0" for x, x_ in zip(read(base + f0), read(base + f0_))]
 tt1 = [x + "\t" + replace(x_) + "\t" + "1" for x, x_ in zip(read(base + f1), read(base + f1_))]
 
-write(tmp + f"{ds}.test.tfidf", tt0 + tt1)
+write(tmp + f"{ds}.test.mask.tfidf", tt0 + tt1)
---
+++ script/job_tfidf.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ds=$1
+
+log_f="../log/$ds-tfidf.log"
+if [ ! -f $log_f  ]; then
+    touch $log_f
+fi
+
+for i in $(seq 0.8 0.02 0.98)
+do
+    echo $i >> $log_f
+    cd /home/zaracs/workspace/baselines/tagger-generator/tag-and-generate-data-prep
+    python src/run.py --data_pth ../data/$ds.tsv --outpath ../data/ --style_0_label P_0 --style_1_label P_9 --is_unimodal False --thresh $i
+
+    cd /home/zaracs/workspace/policy-style-detection/script
+    python eval_tg.py $ds
+
+    python gap.py $ds ../tmp/$ds.test.mask.tfidf >> $log_f
+done
