795eb3ad2b03cbfd91034c29a624b85ea2d9da44
zaracs <whqian001@outlook.com>
2020-12-22 22:18:00 +0800

    none

--- script/extract.py
+++ script/extract.py
@@ -1,30 +1,14 @@
 from fio import read, write
 
-rev_keys = dict(enumerate(read("../data/gyafc/style.test.0")))
-keys = {"".join(rev_keys[k].split()): k for k in rev_keys}
-lines = read("../data/gyafc/style.test.sub")
+model = "ours"
 
-print(f"total test samples: {len(lines)}")
+inp = f"../out/{model}/gyafc_test_ilm.tsf"
+out = f"../out/{model}/gyafc_test_cwsr_ver.tsf"
+idx_f = f"../data/gyafc/cwsr_ver_idx.txt"
 
-# extract from PTO and CWSR
-f1 = "../out/pto/GYAFC_pto.txt"
-f2 = "../out/cwsr/GYAFC_cwsr.txt"
+lines = read(inp)
+idxs = [int(idx) for idx in read(idx_f)]
 
-sample1 = read(f1)
-sample2 = read(f2)
+lines = [lines[id_].strip() for id_ in idxs]
 
-hit, idxs = [], []
-for idx, l in enumerate(lines):
-    if "".join(l.split()).strip() in keys:
-        hit.append((idx, l))
-        idxs.append(str(keys["".join(l.split()).strip()]))
-
-write("../data/gyafc/cwsr_ver_idx.txt", idxs)
-print(f"total hits: {len(hit)}")
-
-# write to style file
-tp1 = ["\t".join([l, sample1[idx], "1"]) for idx, l in hit]
-tp2 = ["\t".join([l, sample2[idx], "1"]) for idx, l in hit]
-
-write("../out/pto/gyafc_test.tsf", tp1)
-write("../out/cwsr/gyafc_test.tsf", tp2)
+write(out, lines)
