1d6df24520ffaf4a736a1439e5a8c3e66081cad3
zaracs <whqian001@outlook.com>
2020-12-27 09:36:10 +0800

    fix run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,3 +1,7 @@
+python main_mask.py yelp inf
+git checkout master
 python main_rank.py yelp test
 python main_insert.py yelp train
 python main_insert.py yelp test
+git checkout double-streams
+cd ../script
